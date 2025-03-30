77b7609757b32e54270d75aa7d8fe7fcc1f37d43
aalikhaann <157011951+aalikhaann@users.noreply.github.com>
2025-03-30 23:33:17 +0500

    Add files via upload

---
+++ Lab8/shop_back/api/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryProductsView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        category_id = self.kwargs['id']
+        return Product.objects.filter(category_id=category_id)
+
